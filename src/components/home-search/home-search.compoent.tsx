f391df06b080234e5e275b99304299e0a34103a9
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2022-12-29 18:59:52 -0800

    Minor UI updates

--- src/components/home-search/home-search.compoent.tsx
+++ src/components/home-search/home-search.compoent.tsx
@@ -4,7 +4,7 @@ const HomeSearch = () => {
     return (
         <div className="home-search">
             <form className="home-search-form">
-                <input className="home-search-input" />
+                <input className="home-search-input" placeholder='Search by Name or Number' />
                 <button className="home-search-button">Q</button>
             </form>
             <div className="home-search-advanced" >Advenced options</div>
